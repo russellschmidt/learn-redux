264b88bd1e78e264f49829a178e30df270f0d446
Russell Schmidt <russell.schmidt@me.com>
2017-05-30 10:53:33 -0700

    add in action to redux-example

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,8 +3,17 @@ var redux = require('redux');
 console.log("Starting redux example");
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
+  console.log('New Action', action);
 
-  return state;
+  switch(action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
@@ -12,3 +21,10 @@ var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log('current state: ', currentState);
+
+store.dispatch({
+  type: "CHANGE_NAME",
+  name: "Bobert"
+});
+
+console.log('Name should be Bobert', store.getState());
