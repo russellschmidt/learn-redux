33dad78ff496640659ae8b923feacf60e6b59cd9
Russell Schmidt <russell.schmidt@me.com>
2017-06-01 11:42:22 -0700

    add axios and location finder

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log("Starting redux example");
 
@@ -90,12 +91,56 @@ var removeMovie = (id) => {
   };
 };
 
+// Map Reducer and Action Generators
+// -----------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  };
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = "http://maps.google.com?q=";
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  })
+}
+
+
 // Combined Reducers
 // -----------------------
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
@@ -107,7 +152,15 @@ var unsubscribe = store.subscribe( () => {
   var state = store.getState();
 
   document.getElementById('app').innerHTML = state.name;
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="'+state.map.url+'" target="_blank">View your location</a>';
+  }
 });
+
+fetchLocation();
 
 store.dispatch(changeName('Boberta'));
 store.dispatch(changeName('Frankinda'));
